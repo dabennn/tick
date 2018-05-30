9b0b837308d0cd6aff22e2fc96f6fdaec22355db
daben <dabennn07@gmail.com>
2018-05-30 17:54:58 +0800

    feat: update test file

--- test/leap-year.test.js
+++ test/leap-year.test.js
@@ -3,9 +3,9 @@ const should = require('should');
 
 describe('test isLeapYear', () => {
   it(`current year is leap year`, () => {
-    Tick.isLeapYear('2018').should.be.false;
-    Tick.isLeapYear(2016).should.be.ok;
-    Tick.isLeapYear('2000').should.be.ok;
-    Tick.isLeapYear(1900).should.be.false;
+    Tick.isLeapYear('2018').should.equal(false);
+    Tick.isLeapYear(2016).should.equal(true);
+    Tick.isLeapYear('2000').should.equal(true);
+    Tick.isLeapYear(1900).should.equal(false);
   });
 })
